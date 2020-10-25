f99d8907a9543962cd24ed0d03a196f571bda3ce
Tobias Bück <youremail@yourdomain.com>
2020-10-25 10:51:48 +0100

    r

--- app.py
+++ app.py
@@ -70,6 +70,8 @@ def get_status_steckdose():
     else:
         return "OFF"
 
+def run():
+    app.run(host="0.0.0.0", port=5001)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001)
